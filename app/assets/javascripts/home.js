4c606d2cf3d2784d956400453f7e887c24d8924c
Dave Allie <dave@daveallie.com>
2015-07-21 21:32:29 +1000

    Added: Favicons and hide menu on start if on mobile

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -90,6 +90,9 @@ $(document).ready(function() {
     toggleMenu();
   });
 
+  if (findBootstrapEnvironment() == 'xs') {
+    toggleMenu();
+  }
   update_spell_count()
 })
 
@@ -196,4 +199,21 @@ function update_headers() {
       $span.addClass('glyphicon-arrow-down')
     }
   })
+}
+
+function findBootstrapEnvironment() {
+  var envs = ['xs', 'sm', 'md', 'lg']
+    , $el = $('<div>')
+
+  $el.appendTo($('body'))
+
+  for (var i = envs.length - 1; i >= 0; i--) {
+    var env = envs[i]
+
+    $el.addClass('hidden-'+env)
+    if ($el.is(':hidden')) {
+      $el.remove()
+      return env
+    }
+  }
 }
