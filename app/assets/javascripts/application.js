4ab44ca0af10fa7de1c07f488da38a439e932c9d
Dave Allie <dave@daveallie.com>
2015-07-31 19:53:11 +1000

    Add inspectlet

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,15 @@
 //= require bootstrap-sprockets
 //= require bootbox.min.js
 
+<!-- Begin Inspectlet Embed Code -->
+window.__insp = window.__insp || [];
+__insp.push(['wid', 116609357]);
+(function() {
+  function __ldinsp(){var insp = document.createElement('script'); insp.type = 'text/javascript'; insp.async = true; insp.id = "inspsync"; insp.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://cdn.inspectlet.com/inspectlet.js'; var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(insp, x); };
+  document.readyState != "complete" ? (window.attachEvent ? window.attachEvent('onload', __ldinsp) : window.addEventListener('load', __ldinsp, false)) : __ldinsp();
+})();
+  <!-- End Inspectlet Embed Code -->
+
 function findBootstrapEnvironment() {
   var envs = ['xs', 'sm', 'md', 'lg']
     , $el = $('<div>')
