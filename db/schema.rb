07ee7bcaba83363fabef15cb74166fee7560cb29
Dave Allie <dave@daveallie.com>
2015-08-18 23:05:15 +1000

    Add nickname to spell books

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150818115152) do
+ActiveRecord::Schema.define(version: 20150818122804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20150818115152) do
     t.integer  "spell_code_map_id"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+    t.string   "nickname"
   end
 
   create_table "spell_code_maps", force: :cascade do |t|
