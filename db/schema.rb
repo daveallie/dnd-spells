338eb72c0f539773339912d2c83d8bd46c698972
Dave Allie <dave@daveallie.com>
2016-07-23 21:55:43 +1000

    Switch back to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20150824110147) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "dnd_classes", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
@@ -46,7 +49,7 @@ ActiveRecord::Schema.define(version: 20150824110147) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
-    t.index ["key"], name: "index_spell_code_maps_on_key", unique: true
+    t.index ["key"], name: "index_spell_code_maps_on_key", unique: true, using: :btree
   end
 
   create_table "spells", force: :cascade do |t|
@@ -79,16 +82,16 @@ ActiveRecord::Schema.define(version: 20150824110147) do
     t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
   end
 
 end
